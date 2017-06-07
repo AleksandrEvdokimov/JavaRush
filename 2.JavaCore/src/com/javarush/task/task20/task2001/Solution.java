74ae44bcd74c279c1377da046d1dd3e95a4d043e
Aleksandr Evdokimov <saneok777@gmail.com>
2017-06-07 17:45:07 +0300

    added new Solutions to git

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2001/Solution.java
@@ -0,0 +1,100 @@
+package com.javarush.task.task20.task2001;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Читаем и пишем в файл: Human
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+
+            File your_file_name = File.createTempFile("/home/saneok777/Yandex.Disk/file_for_test", null);
+            OutputStream outputStream = new FileOutputStream(your_file_name);
+            InputStream inputStream = new FileInputStream(your_file_name);
+
+            Human ivanov = new Human("Ivanov", new Asset("home"), new Asset("car"));
+            ivanov.save(outputStream);
+            outputStream.flush();
+
+            Human somePerson = new Human();
+            somePerson.load(inputStream);
+            //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
+            System.out.println(ivanov.equals(somePerson)); // ????????? непонятно что именно проверить и куда девать результат ?????????
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+
+    public static class Human {
+        public String name;
+        public List<Asset> assets = new ArrayList<>();
+
+        public Human() {
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Human human = (Human) o;
+
+            if (name != null ? !name.equals(human.name) : human.name != null) return false;
+            return assets != null ? assets.equals(human.assets) : human.assets == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (assets != null ? assets.hashCode() : 0);
+            return result;
+        }
+
+        public Human(String name, Asset... assets) {
+            this.name = name;
+            if (assets != null) {
+                this.assets.addAll(Arrays.asList(assets));
+            }
+        }
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            PrintWriter writer = new PrintWriter(outputStream);
+            // Записываем имя объекта
+            writer.println(name);
+            // Перебираем и записываем все имущество чувака название и цену
+            for (Asset asset : assets)
+            {
+                writer.println(asset.getName());
+                writer.println(Double.toString(asset.getPrice()));
+            }
+            writer.flush();
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            name = reader.readLine();
+            String assetName, price;
+            while ((assetName = reader.readLine()) != null & (price = reader.readLine()) != null)
+            {
+                Asset asset = new Asset(assetName);
+                asset.setPrice(Double.parseDouble(price));
+                assets.add(asset);
+            }
+        }
+    }
+}
