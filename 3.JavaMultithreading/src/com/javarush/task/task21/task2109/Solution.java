ba2baafb677ffb5e8870f419383a052e6f44e64f
Aleksandr Evdokimov <saneok777@gmail.com>
2017-06-17 23:12:48 +0300

    added new Solutions to git

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,105 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+
+        /*@Override
+        protected A clone() throws CloneNotSupportedException {
+            return new A(getI(), getJ());
+        }*/
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            A a = (A) o;
+
+            if (i != a.i) return false;
+            return j == a.j;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = i;
+            result = 31 * result + j;
+            return result;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+
+            B b = (B) o;
+
+            return name != null ? name.equals(b.name) : b.name == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = super.hashCode();
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected C clone() throws CloneNotSupportedException {
+            return new C(getI(), getJ(), getName());
+        }
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+       /* A a = new A(1, 1);
+        B b = new B(2, 2, "classB");
+        C c = new C(3, 3, "classC");
+
+        A aa =  a.clone();
+
+        C cc =  c.clone();*/
+    }
+}
