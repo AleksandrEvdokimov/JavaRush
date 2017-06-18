de77f4110e69bb38ff85036f1491558b6be0c90f
Aleksandr Evdokimov <saneok777@gmail.com>
2017-06-18 22:49:07 +0300

    added new Solutions to git

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.List;
+
+/**
+ * Created by saneok on 18.06.17.
+ */
+public class Hippodrome {
+
+    private List<Horse> horses;
+
+    public Hippodrome (List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
